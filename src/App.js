97aaa021949dded0fa3925f6964c16717124202e
Jesse Rauha <jesse.rauha@gmail.com>
2019-02-16 00:44:00 +0200

    Initial UX consepts

--- src/App.js
+++ src/App.js
@@ -2,24 +2,63 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Camera from 'react-html5-camera-photo';
+import 'react-html5-camera-photo/build/css/index.css';
+
+function ResultFooter({ label }) {
+  switch (label) {
+    case 'pirkka3':
+      return (
+        <header className="App-footer App-footer--failure">
+          <div>Pirkka III-olut 4,5%</div>
+          <button className='btn'>Lue lisää</button>
+        </header>
+      )
+    case 'none':
+      return (
+        <header className="App-footer App-footer--success">
+          <div>Ei Pirkka!</div>
+        </header>
+      );
+    default:
+        return <header></header>;
+  }
+}
+
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      viewId: 'home',
+      currentLabel: null,
+      image: null,
+
+    };
+  }
+
+  onTakePhoto(dataUri) {
+    console.log('takePhoto', dataUri);
+
+    if (Math.random() >= 0.5) {
+      this.setState({currentLabel: 'none'})
+    } else {
+      this.setState({currentLabel: 'pirkka3'})
+    }
+  }
+
   render() {
+    const { currentLabel } = this.state;
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <header className="App-header"><img className="App-logo" src="https://via.placeholder.com/64/09f/fff.png%20C/O%20https://placeholder.com/"></img></header>
+        <div className="App-content">
+          <Camera
+            onTakePhoto={(dataUri) => { this.onTakePhoto(dataUri); }}
+          />
+        </div>
+        <ResultFooter label={currentLabel} />
       </div>
     );
   }
