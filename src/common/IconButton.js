75e98aa777c247ebf7731944f6f41fd61014c1b9
Jesse Rauha <jesse.rauha@gmail.com>
2019-02-16 10:30:32 +0200

    Add hidden property to Button component

--- src/common/IconButton.js
+++ src/common/IconButton.js
@@ -2,5 +2,5 @@ import React from 'react';
 import './IconButton.css';
 
 export default function IconButton({ hidden, className, onClick, icon }) {
-    return <button hidden={hidden} role="button" onClick={onClick} className={`IconButton ${className ? className : ''}`}><i className={`fas fa-${icon} fa-2x`}></i></button>
+    return <button role="button" onClick={onClick} className={`IconButton ${className ? className : ''} ${hidden ? 'IconButton--hidden' : ''}`}><i className={`fas fa-${icon} fa-2x`}></i></button>
 }
