31a1d09f48c45df7594c7d04fba9a18744b1e6a8
Jesse Rauha <jesse.rauha@gmail.com>
2019-02-16 10:23:11 +0200

    Minor UX tweaks

--- src/common/IconButton.js
+++ src/common/IconButton.js
@@ -2,5 +2,5 @@ import React from 'react';
 import './IconButton.css';
 
 export default function IconButton({ hidden, className, onClick, icon }) {
-    return <div hidden={hidden} role="button" onClick={onClick} className={`IconButton ${className ? className : ''}`}><i className={`fas fa-${icon} fa-2x`}></i></div>
+    return <button hidden={hidden} role="button" onClick={onClick} className={`IconButton ${className ? className : ''}`}><i className={`fas fa-${icon} fa-2x`}></i></button>
 }
