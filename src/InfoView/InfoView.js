3e85ec9d91d1307400bf3c107bb1c6c8de560862
Jesse Rauha <jesse.rauha@gmail.com>
2019-02-16 03:42:39 +0200

    New info view

---
+++ src/InfoView/InfoView.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './InfoView.css';
+
+// TODO: load data dynamically
+
+const coverStyle = {
+  backgroundImage: "url('http://1.bp.blogspot.com/-BbbJXf3HnkI/ToHMXghDy_I/AAAAAAAAAs0/fiYVm2f4kiE/s1600/IMG_2874.JPG')"
+}
+
+class InfoView extends React.Component {
+
+  render() {
+    return (
+      <div className="InfoView">
+        <div className="InfoView-close"><i class="fas fa-times fa-2x"></i></div>
+        <div className="InfoView-cover" style={coverStyle}></div>
+        <div className="InfoView-content">
+          <h1 className="InfoView-title">Pirkka III-olut 4,5%</h1>
+          <div className="InfoView-description">Pirkka olut on raikas suomalainen lagerolut, joka valmistetaan kotimaisesta ohramaltaasta ja jota täydentää hieno aromi-humalointi. Se nautitaan kylmänä.</div>
+          <div className="InfoView-rating">
+            <i class="fa fa-star" aria-hidden="true"></i>
+            <i class="fa fa-star" aria-hidden="true"></i>
+            <i class="fa fa-star" aria-hidden="true"></i>
+            <i class="fa fa-star" aria-hidden="true"></i>
+            <i class="fa fa-star-half-alt" aria-hidden="true"></i>
+          </div>
+        </div>
+      </div>
+    );
+  }
+};
+
+export default InfoView;
